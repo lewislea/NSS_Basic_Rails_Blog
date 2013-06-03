7159a2dffd310cd2951f1ef9026876ec10a52792
lewislea <wlewislea@gmail.com>
2013-06-03 12:13:07 -0500

    update test folder, app folder, config. eliminate fixtures

--- Gemfile
+++ Gemfile
@@ -7,6 +7,11 @@ gem 'rails', '3.2.13'
 
 gem 'sqlite3'
 
+group :test do
+  gem 'capybara'
+  gem 'selenium-webdriver'
+end
+
 
 # Gems used only for assets and not required
 # in production environments by default.
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,9 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+
 require 'capybara/rails'
+Capybara.default_driver = :selenium
 
 class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
@@ -11,3 +13,4 @@ class ActionDispatch::IntegrationTest
   # Make the Capybara DSL available in all integration tests
   include Capybara::DSL
 end
+
